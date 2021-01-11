c17118fb884196db0cc154c3c332cd51d5da84e7
Birgit <birgitkulli@gmail.com>
2021-01-11 11:01:02 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Birgit Kulli and is {""}
+        <a href="https://github.com/KBirgit/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
