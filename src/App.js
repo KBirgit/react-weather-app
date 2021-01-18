04fcca2c40c72f8bbc76899e37f6f639e4e3860d
Birgit <birgitkulli@gmail.com>
2021-01-18 09:20:03 +0200

    Added className container

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Birgit Kulli and is {""}
-        <a href="https://github.com/KBirgit/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Birgit Kulli and is {""}
+          <a
+            href="https://github.com/KBirgit/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
